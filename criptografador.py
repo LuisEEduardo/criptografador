c7de3ff96632f857095c510c041b4536795b184e
LuisEEduardo <luiseduardolemos282@gmail.com>
2021-08-03 20:34:26 -0300

    criando um encriptador e descriptografador

--- criptografador.py
+++ criptografador.py
@@ -1,4 +1,4 @@
-
+from cryptography.fernet import Fernet
 # EXEMPLO DE ENCRIPTAÇÃO E DESCRIPTAÇÃO
 """
 key = Fernet.generate_key()
@@ -6,3 +6,14 @@ cipher_suite = Fernet(key)
 cipher_text = cipher_suite.encrypt(b"A really secret message. Not for prying eyes.")
 plain_text = cipher_suite.decrypt(cipher_text)
 """
+
+# Gerando chave 
+chave = Fernet.generate_key()
+
+cifra = Fernet(chave)
+
+criptografando = cifra.encrypt(b"Mensagem secreta")
+print(criptografando)
+
+descriptografador = cifra.decrypt(criptografando)
+print(descriptografador)
