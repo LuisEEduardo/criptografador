0acd616fc8df546d83d2bd75106c7e20c9cd86fd
LuisEEduardo <luiseduardolemos282@gmail.com>
2021-08-05 13:36:44 -0300

    colocando o decrypt para descriptografar usando o path dos arquivos

--- decrypt.py
+++ decrypt.py
@@ -6,11 +6,30 @@ def clear():
 
 def descriptografando(): 
     chave = input("Insira a chave: ")
-    texto = input('Insira o texto: ')
-    texto_decrypt = Fernet(chave.encode()).decrypt(texto.encode())
+    print("""
+    Opção: 
+    1 - Para inserir o texto no terminal
+    2 - Para inserir o caminho do arquivo .txt
+    """)
+
+    op = int(input("Insira a opção: "))
+    if op == 1:
+        texto = input('Insira o texto criptografado: ')
+        texto_decrypt = Fernet(chave.encode()).decrypt(texto.encode())
+    elif op == 2:
+        print()
+        nome_arquivo = input("Insira o caminho do arquivo: ")
+        file = open(nome_arquivo, 'rb')
+        texto = file.read()
+        file.close()
+        texto_decrypt = Fernet(chave.encode()).decrypt(texto)
+    else: 
+        texto_decrypt = "Error"
     clear()
     print('-=' * 30)
     print(f"Texto descriptografado: \n{texto_decrypt}")
     print('-=' * 30)
+
+
 
 descriptografando()
