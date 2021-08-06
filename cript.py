65b6eb09d2c2b65429347134e0d3d545e95a74e9
LuisEEduardo <luiseduardolemos282@gmail.com>
2021-08-06 13:34:59 -0300

    atualizando o arquivo cript

--- cript.py
+++ cript.py
@@ -7,10 +7,14 @@ def gerar_chave():
 
 
 def encriptando(): 
-    t = texto()
+    t, opcao, n = texto()
     key = gerar_chave()
     cipher_suite = Fernet(key)
     texto_encrypt = cipher_suite.encrypt(t.encode())
+    if opcao == 2: 
+        file = open(n, "wb")
+        file.read(texto_encrypt)
+        file.close()
     return key, texto_encrypt
 
 
@@ -21,15 +25,16 @@ def texto():
     2 - Para colocar o caminho do arquivo .txt
     """)
     op = int(input('Insira a opção: '))
+    nome_arquivo = "sem"
     if op == 1: 
         texto = input('Insira o texto: ')
-        return texto
+        return texto, op, nome_arquivo 
     elif op == 2: 
         nome_arquivo = input("Insira o caminho do arquivo: ")
         file = open(nome_arquivo, 'r')
         arquivo_ref = file.read()
         file.close()
-        return arquivo_ref
+        return arquivo_ref, op, nome_arquivo
     else: 
         print("Error")
 
