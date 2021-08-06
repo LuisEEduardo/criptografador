13900f0184811863e7f1e6fdd82b897ce07d0c4a
LuisEEduardo <luiseduardolemos282@gmail.com>
2021-08-06 15:01:52 -0300

    refatorando o decrypt

--- decrypt.py
+++ decrypt.py
@@ -1,27 +1,35 @@
 from cryptography.fernet import Fernet
 
+
 def clear():
     import os
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def descriptografando(): 
+
+def inserir_texto(): 
+    clear()
     chave = input("Insira a chave: ")
-    print("""
-    Opção: 
-    1 - Para inserir o texto no terminal
-    2 - Para inserir o caminho do arquivo .txt
-    """)
+    print("Opção: \n1 - Para inserir o texto no terminal \n2 - Para inserir o caminho do arquivo .txt")
 
     op = int(input("Insira a opção: "))
     if op == 1:
         texto = input('Insira o texto criptografado: ')
-        texto_decrypt = Fernet(chave.encode()).decrypt(texto.encode())
     elif op == 2:
         print()
         nome_arquivo = input("Insira o caminho do arquivo: ")
         file = open(nome_arquivo, 'rb')
         texto = file.read()
         file.close()
+    else: 
+        texto = "Error"
+    return chave, texto, op
+
+
+def descriptografando(): 
+    chave, texto, opcao = inserir_texto()
+    if opcao == 1:
+        texto_decrypt = Fernet(chave.encode()).decrypt(texto.encode())
+    elif opcao == 2:
         texto_decrypt = Fernet(chave.encode()).decrypt(texto)
     else: 
         texto_decrypt = "Error"
